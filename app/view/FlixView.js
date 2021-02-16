f4c879fcf1e9713460a87f0864426a4db8310e82
giuliabriao <briaogiulia@gmail.com>
2021-02-16 18:53:43 -0300

    Corrigir bug do botao no modal de erro e de filme

--- app/view/FlixView.js
+++ app/view/FlixView.js
@@ -9,11 +9,14 @@ class FlixView{
         this.genero = document.querySelector('.genero');
         this.sinopse = document.querySelector('.sinopse');
         this.imdb = document.querySelector('.imdb');
+        this.divImagem = document.querySelector('.div-imagem');
+        this.divSinopse = document.querySelector('.sinopses');
+        this.divBotao = document.querySelector(".div-botao-watchnow");
 
     };
 
     modal(objeto){
-
+        
         this.title.textContent = objeto.Title;
         this.poster.setAttribute("src", objeto.Poster);
         this.year.textContent = `Year: ${objeto.Year}`;
@@ -22,11 +25,21 @@ class FlixView{
         this.imdb.textContent = `IMDB Rating: ${objeto.imdbRating}`;
         this.sinopse.textContent = objeto.Plot;
         this.imdb.textContent = objeto.imdbRating;
-        this.poster.classList.remove('w-75');
+        
+        this.poster.classList.remove('w-100');
+        this.divImagem.classList.remove("border-end");
+        this.divImagem.classList.remove("border-light");
+        this.divSinopse.classList.remove("border");
+        this.divSinopse.classList.remove("border-light");
+        
 
         if(objeto.Response == "False"){
             this.poster.setAttribute("src", '../imgs/error.jpg');
-            this.poster.classList.add('w-75');
+            this.poster.classList.add('w-100');
+            this.year.textContent = "";
+            this.runTime.textContent = "";
+            this.genero.textContent = "";
+            this.imdb.textContent = "";
         }
 
     };
