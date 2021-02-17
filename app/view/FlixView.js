10a253d816c160b0490c5e84fc7264b396088d1c
giuliabriao <briaogiulia@gmail.com>
2021-02-17 15:42:33 -0300

    Modal de Quem Somos feito, acabamentos e alguns bugs consertados

--- app/view/FlixView.js
+++ app/view/FlixView.js
@@ -11,7 +11,7 @@ class FlixView{
         this.imdb = document.querySelector('.imdb');
         this.divImagem = document.querySelector('.div-imagem');
         this.divSinopse = document.querySelector('.sinopses');
-        this.divBotao = document.querySelector(".div-botao-watchnow");
+        this.botaoWatchNow = document.querySelector("#button-watchnow");
 
     };
 
@@ -24,14 +24,13 @@ class FlixView{
         this.genero.textContent = `Genre: ${objeto.Genre}`;
         this.imdb.textContent = `IMDB Rating: ${objeto.imdbRating}`;
         this.sinopse.textContent = objeto.Plot;
-        this.imdb.textContent = objeto.imdbRating;
         
         this.poster.classList.remove('w-100');
-        this.divImagem.classList.remove("border-end");
-        this.divImagem.classList.remove("border-light");
-        this.divSinopse.classList.remove("border");
-        this.divSinopse.classList.remove("border-light");
-        
+        this.divImagem.classList.add("border-end");
+        this.divImagem.classList.add("border-light");
+        this.divSinopse.classList.add("border");
+        this.divSinopse.classList.add("border-light");
+        this.botaoWatchNow.classList.remove("invisible");
 
         if(objeto.Response == "False"){
             this.poster.setAttribute("src", '../imgs/error.jpg');
@@ -40,7 +39,11 @@ class FlixView{
             this.runTime.textContent = "";
             this.genero.textContent = "";
             this.imdb.textContent = "";
+            this.divImagem.classList.remove("border-end");
+            this.divImagem.classList.remove("border-light");
+            this.divSinopse.classList.remove("border");
+            this.divSinopse.classList.remove("border-light");
+            this.botaoWatchNow.classList.add("invisible")
         }
-
     };
 };
